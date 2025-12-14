925cd2ea165358d25ccc23d6bd764aa4e3256493
Sathwik Annaldesh <93048377+Sathwik041@users.noreply.github.com>
2025-12-14 10:18:00 +0530

    Enhance PassingWill contract with recipient and time logic
    Added functionality for time-based transfer to recipient.

--- WillContract/WillContract.sol
+++ WillContract/WillContract.sol
@@ -3,22 +3,44 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PassingWill{
-    address owner;
-     address recipient;
+     address owner;
+     address payable recipient;
+     //need to have variables for keepig the time till when owner not getback,transfer to recipient
+     uint StartTime;
+     uint LastVisited;
+     uint FiveYears;
 
-    constructor(){
+     
+    constructor(address payable _recipient){
         owner=msg.sender;
-    
-       
-    }
-
-    function setInitializing(address _person) public{
-        owner=_person;
-
-    }
-
-    function setRecipient(address _recipient) public{
         recipient=_recipient;
+        StartTime=block.timestamp;
+        FiveYears=5*365*24*60;
+        LastVisited=block.timestamp;       
+    }
+
+    modifier onlyOwner(){
+        require(owner==msg.sender,"Aap nahi mera Maalik!");
+        _;
+    }
+
+    modifier onlyRecipient(){
+        require(msg.sender==recipient,"Hattt!Kon hai re thu");
+        _;
+    }
+
+    function deposit() public payable onlyOwner{
+        LastVisited=block.timestamp;
+    }
+
+    function ping() public onlyOwner{ //reminding owner is alive
+        LastVisited=block.timestamp;
     } 
+
+    function claim() external onlyRecipient{
+        
+        require(LastVisited<block.timestamp-FiveYears);
+        payable(recipient).transfer(address(this).balance);
+    }
 
 }
